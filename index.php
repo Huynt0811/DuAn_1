b462eab8767b8e70955929f7039701b5674b11c0
Huynt0811 <92984865+Huynt0811@users.noreply.github.com>
2021-11-25 09:16:40 +0700

    prd detail

--- index.php
+++ index.php
@@ -4,14 +4,30 @@
     include "./model/product.php";
     include "./model/taikhoan.php";
     include "./model/thongke.php";
-    // include "./model/user.php";
     include "./model/comment.php";
     include "./view/header.php";
-
     $prdnew = loadall_product_home();
-
+    
+    
+    // home
     if ((isset($_GET['act']))&&($_GET['act']!="")) {
-        $art = $_GET['act'];
+        $act = $_GET['act'];
+        switch($act){
+            case 'prd_detail':
+                if(isset($_GET['idprd'])&&$_GET['idprd']>0){
+                    $id_product = $_GET['idprd'];
+                    $oneprd = loadone_product($id_product);
+                    extract($oneprd);
+                    $prd_same = load_product_cungloai($id_product, $id_category);
+                    include "./view/prd_detail.php";
+                }else{
+                    include "./view/home.php";
+                }
+                break;
+            default:
+                include "./view/home.php";
+                break;
+        }
     }else{
         include "./view/home.php";
     }
--- view/home.php
+++ view/home.php
@@ -1,3 +1,58 @@
+<aside>
+                <div class="container">
+                    <div class="banner">
+                        <div class="slideshow-container">
+
+                            <!-- Full-width images with number and caption text -->
+                            <div class="mySlides fade">
+                                <!-- <div class="numbertext">1 / 3</div> -->
+                                <img src="./images/banner.jpg" style="width:100%">
+                                <!-- <div class="text">Caption Text</div> -->
+                            </div>
+
+                            <div class="mySlides fade">
+                                <!-- <div class="numbertext">2 / 3</div> -->
+                                <img src="./images/banner.jpg" style="width:100%">
+                                <!-- <div class="text">Caption Two</div> -->
+                            </div>
+
+                            <!-- <div class="mySlides fade">
+                                <div class="numbertext">3 / 3</div>
+                                <img src="img3.jpg" style="width:100%">
+                                <div class="text">Caption Three</div>
+                            </div> -->
+
+                            <!-- Next and previous buttons -->
+                            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                        </div>
+                        <br>
+
+                        <!-- The dots/circles -->
+                        <!-- <div style="text-align:center">
+                            <span class="dot" onclick="currentSlide(1)"></span>
+                            <span class="dot" onclick="currentSlide(2)"></span>
+                            <span class="dot" onclick="currentSlide(3)"></span>
+                    </div> -->
+                        <script>
+                            var slideIndex = 0;
+                            showSlides();
+
+                            function showSlides() {
+                                var i;
+                                var slides = document.getElementsByClassName("mySlides");
+                                for (i = 0; i < slides.length; i++) {
+                                    slides[i].style.display = "none";
+                                }
+                                slideIndex++;
+                                if (slideIndex > slides.length) {
+                                    slideIndex = 1
+                                }
+                                slides[slideIndex - 1].style.display = "block";
+                                setTimeout(showSlides, 2000); // Change image every 2 seconds
+                            }
+                        </script>
+                    </div>
 <div class="title-top">
                         <p>New Arrivals</p>
                         <a href="#">Xem Thêm</a>
@@ -9,12 +64,13 @@
                             $a=4;
                             foreach($prdnew as $prd){
                                 extract($prd);
+                                $linkprd ="index.php?act=prd_detail&idprd=".$id_product;
                                 $img_path = "upload/".$img;
                                 if($i==1 || $i==5 || $i==8){
                                     echo '<div class="product-block">';
                                 }
                                 echo '<div class="product-list mr">
-                                        <a href=""><img src="'. $img_path .'" alt=""></a>
+                                        <a href="'. $linkprd .'"><img src="'. $img_path .'" alt=""></a>
                                         <p  href="">' . $name_product . '</p>
                                         <div class="button-add">
                                             <button type="submit" class="action">Mua ngay</button>
@@ -194,4 +250,4 @@
                     </div>
                 </div>
 
-                <footer>
+                

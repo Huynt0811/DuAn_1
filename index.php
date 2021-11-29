6f48e91fdc556a319ed2d383f547c48cac42d73b
Huynt0811 <92984865+Huynt0811@users.noreply.github.com>
2021-11-30 00:50:36 +0700

    gio hàng

--- index.php
+++ index.php
@@ -6,55 +6,101 @@ include "./model/category.php";
 include "./model/product.php";
 include "./model/taikhoan.php";
 include "./model/thongke.php";
-// include "./model/user.php";
 include "./model/comment.php";
+$prdnew = loadall_product_home();
+$lstcate = loadall_category();
 include "./view/header.php";
 
-$prdnew = loadall_product_home();
+
 
 if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($_GET['act']) {
+        case 'prd':
+            if(isset($_GET['id_cate'])&&$_GET['id_cate']>0){
+                $id_cate = $_GET['id_cate'];
+                $lstprd = loadall_product("", $id_cate);
+                include "./view/product.php";
+            }else{
+                include "./view/home.php";
+            }
+            break;
+        case 'prd_detail':
+            if(isset($_GET['idprd'])&&$_GET['idprd']>0){
+                $id_product = $_GET['idprd'];
+                $oneprd = loadone_product($id_product);
+                extract($oneprd);
+                $prd_same = load_product_cungloai($id_product, $id_category);
+                include "./view/prd_detail.php";
+            }else{
+                include "./view/home.php";
+            }
+            break;
         case 'dangky':
             if (isset($_POST['dangky']) && ($_POST['dangky'])) {
-                $username = $_POST['user'];
-                $password = $_POST['password'];
-                $email = $_POST['email'];
-                $address = $_POST['address'];
-                insert_taikhoan($username, $password, $email, $address);
-                $thongbao = "Đăng ký thành công!";
+                if (empty($_POST["email"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập email</span>";
+                } else if (empty($_POST["user"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập tên đăng nhập</span>";
+                } else if (empty($_POST["password"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập mật khẩu</span>";
+                } else if (empty($_POST["address"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập địa chỉ</span>";
+                } else {
+                    $email = $_POST['email'];
+                    $username = $_POST['user'];
+                    $password = $_POST['password'];
+                    $address = $_POST['address'];
+                    insert_taikhoan($username,$password,$email,$address);
+                    $thongbao = "Đăng kí thành công. Vui lòng đăng nhập";
+                }
             }
             include "./view/user/signup.php";
             break;
         case 'dangnhap':
             if (isset($_POST['dangnhap']) && ($_POST['dangnhap'])) {
-                $username = $_POST['user'];
-                $password = $_POST['password'];
-                $checkuser = checkuser($username, $password);
-                if (is_array($checkuser)) {
-                    $_SESSION['user'] = $checkuser;
-                     //header('Location: index.php');
+                if (empty($_POST["user"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập tên đăng nhập</span>";
+                } else if (empty($_POST["password"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập mật khẩu</span>";
                 } else {
-                    $thongbao = "Tài khoản không tồn tại. Vui lòng đăng ký!";
+                    $username = $_POST['user'];
+                    $password = $_POST['password'];
+                    $checkuser = checkuser($username, $password);
+                    if (is_array($checkuser)) {
+                        $_SESSION['user'] = $checkuser;
+                        //header('Location: index.php');
+                    } else {
+                        $thongbao = "Tài khoản không tồn tại. Vui lòng đăng ký!";
+                    }
                 }
             }
             include "./view/user/signin.php";
             break;
-        
-        case 'thongtin':
-            include "./view/user/info.php";
-            break;
 
         case 'edit_tk':
-            if(isset($_POST['capnhat']) && ($_POST['capnhat'])){
+            if (isset($_POST['capnhat']) && ($_POST['capnhat'])) {
+                if (empty($_POST["email"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập email</span>";
+                } else if (empty($_POST["user"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập tên đăng nhập</span>";
+                } else if (empty($_POST["user"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập mật khẩu</span>";
+                } else if (empty($_POST["phone"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập số điện thoại</span>";
+                } else if (empty($_POST["birthdate"])) {
+                    $thongbao = "<span style='color:red;'>Error: Chưa nhập ngày tháng năm sinh</span>";
+                } else {
+                    $thongbao = "Cập nhật thành công!";
+                }
                 $username = $_POST['user'];
                 $password = $_POST['password'];
                 $email = $_POST['email'];
                 $phone = $_POST['phone'];
                 $birthdate = $_POST['birthdate'];
-                update_user($id_user,$username,$password,$email,$phone,$birthdate);
+                update_user($id_user, $username, $password, $email, $phone, $birthdate);
                 // header('Location:index.php?act=edit.php');
-                $thongbao = "Cập nhật thành công!";
+
             }
             include "./view/user/edit.php";
             break;
@@ -74,7 +120,10 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
 
         case 'dangxuat':
             session_unset();
-            // header('Location: index.php');
+            header('Location: index.php');
+            break;
+        case 'cart':     
+            include "./view/cart/cart.php";
             break;
         default:
             # code...
@@ -84,13 +133,4 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
 } else {
     include "./view/home.php";
 }
-if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
-    $act = $_GET['act'];
-    switch ($_GET['act']){
-        case 'dangnhap':
-            break;
-            default:
-            include "./view/footer.php";
-    }
-
-}
+include "./view/footer1.php";
--- view/prd_detail.php
+++ view/prd_detail.php
@@ -1,81 +1,96 @@
 <?php
-    extract($oneprd);
-    $img_path = "upload/".$img;
+extract($oneprd);
+$img_path = "upload/" . $img;
+foreach ($lstcate as $cate) {
+    extract($cate);
+    if($id_category==$id_cate){
+        $namecate = $name_category;
+        break;
+    }
+}
 ?>
 <aside>
-    <div class="back-To-Top">
-        <a href="index.php"><i class="fas fa-arrow-left"></i> Back to top</a>
-    </div>
-    <div class="show-product-details">
-        <div class="show-product-details-img">
-            <div class="slide-img">
-                <img src="<?=$img_path?>" alt="">
-
-            </div>
-
+    <div class="container">
+        <div class="back-To-Top">
+            <a href="index.php"><i class="fas fa-arrow-left"></i> Back to top</a>
         </div>
-        <div class="show-product-details-right">
-            <div class="product-name ml mb">
-                <h3><?=$name_product?></h3>
+        <form action="./view/cart.php" method="POST" class="show-product-details" enctype="multipart/form-data">
+            <input type="hidden" name="priceprd" value="<?=$price?>">
+            <input type="hidden" name="imgprd" value="<?=$img_path?>">
+            <input type="hidden" name="nameprd" value="<?=$name_product ?>">
+            <input type="hidden" name="namecate" value="<?=$namecate?>">
+            <div class="show-product-details-img">
+                <div class="slide-img">
+                    <img src="<?= $img_path ?>" alt="">
+                </div>
             </div>
-
-            <div class="product-price ml mb">
-                <span><?=$price?> đ</span>
-                <!-- <del>650.000 đ</del> -->
+            <div class="show-product-details-right">
+                <div class="product-name ml mb">
+                    <h3><?= $name_product ?></h3>
+                </div>
+                <div class="product-price ml mb">
+                    <span><?= $price ?> đ</span>
+                </div>
+                
+                <div class="cart-plus-minus product-quantity ml mb" data-target="amount-1">
+                    <button class="btn cart-minus-1">
+                        <i class='fas fa-minus'></i>
+                    </button>
+                    <input style="width: 150px; text-align: center;" type="number" name="soluong" id="amount-1" value="1" min="1" max="300">
+                    <button class="btn cart-plus-1">
+                        <i class='fas fa-plus'></i>
+                    </button>
+                </div>
+                <div class="btn-add-to-cart">
+                    <button type="submit" name="addcart" class="action add-to-cart">Thêm vào giỏ</button>
+                </div>
+                <div class="btn-buy-now">
+                    <button type="submit" name="btn-buy-now"><strong><a href="">Mua Ngay</a></strong></button>
+                </div>
+                <div class="product-desciption">
+                    <p><? $mota ?></p>
+                </div>
             </div>
-
-            <div class="product-size ml mb">
-                <label for="s">S</label>
-                <label for="s">M</label>
-                <label for="s">L</label>
-                <label for="s">XL</label>
-            </div>
+        </form>
+        <!-- <div class="show-product-details">
             
-            <div class="cart-plus-minus product-quantity ml mb" data-target="amount-1">
-                <button class="btn cart-minus-1">
-                    <i class='fas fa-minus'></i>
-                </button>
-                <input style="width: 150px; text-align: center;" type="number" id="amount-1" value="1" min="1" max="300">
-                <button class="btn cart-plus-1">
-                    <i class='fas fa-plus'></i>
-                </button>
-            </div>
-            
 
-            <div class="btn-add-to-cart">
-                <button type="submit" name="btn-add-to-cart"><strong><a href="">Thêm Vào Giỏ</a></strong></button>
-            </div>
-
-            <div class="btn-buy-now">
-                <button type="submit" name="btn-buy-now"><strong><a href="">Mua Ngay</a></strong></button>
-            </div>
-
-            <div class="product-desciption">
-                <p><?$mota?></p>
-            </div>
-
-        </div>
-
-    </div>
-    <div class="products-related">
-        <h1>SẢN PHẨM LIÊN QUAN</h1>
-        <div class="product-block">
-            <?php
-            
-                if(is_array($prd_same)){
-                    foreach($prd_same as $prd_same){
+        </div> -->
+        <div class="products-related">
+            <h1>SẢN PHẨM LIÊN QUAN</h1>
+            <div class="product-block">
+                <?php
+                    
+                if (is_array($prd_same)) {
+                    $lq = 0;
+                    foreach ($prd_same as $prd_same) {
                         extract($prd_same);
-                        echo '<div class="product-list mr">
-                                    <a href=""><img src="./images/HADES NEW BALANCE TROUSER PANTS.jpg" alt=""></a>
-                                    <a href="">'. $name_product .'</a>
-                                    <div class="button-add">
+                        $img_path_lq = "upload/".$img;
+                        $linkprd ="index.php?act=prd_detail&idprd=".$id_product;
+                        if($lq<=4){
+                            $lq++;
+                            echo '<div class="product-list mr">
+                                    <a href="'.$linkprd.'"><img src="'.$img_path_lq.'" alt=""></a>
+                                    <a href="">' . $name_product . '</a>
+                                    <form action="./view/cart.php" method="POST" class="button-add" enctype="multipart/form-data">
                                         <button type="submit" class="action">Mua ngay</button>
-                                        <button type="submit" class="action add-to-cart">Thêm vào giỏ</button>
-                                    </div>
-                                    <p>450,000₫</p>
+                                        <button type="submit" name="addcart" class="action add-to-cart">Thêm vào giỏ</button>
+                                        <input type="hidden" name="priceprd" value="' . $price . '">
+                                        <input type="hidden" name="imgprd" value="'. $img_path_lq .'">
+                                        <input type="hidden" name="nameprd" value="' . $name_product . '">
+                                        <input type="hidden" name="soluong" value="1">
+                                        <input type="hidden" name="namecate" value="'.$namecate.'">
+                                    </form>
+                                    
+                                    <p>'.$price.'</p>
                             </div>';
+                            
+                        }
+                        
                     }
                 }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
+</aside>
